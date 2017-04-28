e8a8fa2b9afcd3b1279b7ea18cdb18f6aa9a52c5
Melvin Gruschow <melvin.f.gruschow@gmail.com>
2017-04-28 08:38:27 -0700

    base files and folders

---
+++ src/SonOfCod/Models/SonOfCodDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SonOfCod.Models
+{
+    public class SonOfCodDbContext : IdentityDbContext<User>
+    {
+        public SonOfCodDbContext()
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=SonOfCod;integrated security=True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
